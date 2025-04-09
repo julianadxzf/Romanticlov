25d454df856b727049f5b0948c4119aee49e63c1
julianadxzf <julianacaballero038@gmail.com>
2025-04-08 23:53:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Cuando el botón sea presionado
+document.getElementById("surpriseButton").addEventListener("click", function() {
+    // Mostrar el mensaje sorpresa
+    document.getElementById("specialMessage").style.display = "block";
+});
